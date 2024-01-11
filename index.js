c573fa3c3fa7ae3960c950946d497215087aadeb
Harbinger4181 <125174176+Harbinger4181@users.noreply.github.com>
2024-01-11 18:22:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,147 @@
+    // Language content
+    const languages = {
+        en: {
+          TRANSLintro1: "Hi, I'm Walid",
+          TRANSLintro2: "As a fourth-year student at the University of Ottawa, I have strived to diversify my range of competencies, and have dabbled in a few different fields, such as aerospace, naval and telecommunications engineering. I invite you to consult my profile - it is an extension of my CV, and hopefully, a more engaging version of it.",
+          
+          TranslEduc1: "Education",
+          TranslEduc2: "While this high school is typically reserved to students from affluent families, I was selected among many other candidates for a full scolarship. Apparently, I had aced the competition and became the sole winner of the prize. Throughout the years, I have accumulated many honorific mentions due to my grades, and also other recompenses for participating in the volleyball team.",
+          TranslEduc3: "University of Ottawa",
+          TranslEduc4: "Due to my grades, I was given the option to skip CÉGEP altogether and enroll immediately in an Ontario university following my graduation. I have been studying at uOttawa since as an electrical engineer and have been able to experience many different internships.",
+
+          ///////////////////////
+         
+          TranslWork1: "Work Experience",
+
+          TranslWork2: "Canadian Coast Guard (CCG)",
+          TranslWork3: "The Canadian Coast Guard is a federal department overseeing the design and conception of the Canadian fleet. It aims to protect the maritime ecosystem and Canada's naval sovereignty.",
+          TranslWork4: "Assist in the construction and updating of ships' electrical infrastructure",
+          TranslWork5: "Explore research topics around renewable energy and potential energy sources for future ships",
+          TranslWork6: "Analyze and validate official documents while learning about new concepts",
+          TranslWork7: "Discuss various advancements in a multitude of projects with the CCG engineering group",
+          
+          TranslWork8: "Bombardier Aviation is a leading enterprise in the private jet market, manufacturing planes for high-profile clients for decades. I worked in the Electromagnetic Compatibility (EMC) team.",
+          TranslWork9: "Contribute to the validation/development of tools to simulate and analyze CEM data",
+          TranslWork10: "Use EMC software such as Altair Feko to simulate electromagnetic interference between cables and the effectiveness of anti-interference shielding",
+          TranslWork11: "Analyze data and test the results of current research projects",
+          TranslWork12: "Participate in knowledge sharing meetings and conferences with the EMC team",
+
+          TranslWork13: "Innovation, Science and Economic Development (ISED)",
+          TranslWork14: "ISED is another federal department which undertakes telecommunications tasks, notably the funding and overseeing of Internet expansion projects in order to serve the furthest Canadian regions and villages.",
+          TranslWork15: "Introduction to VBA - modifying and adding functions in database tools in Microsoft Excel/Access. Also helped others debug and solve coding issues.",
+          TranslWork16: "Project reviews - deliverables, project sites, coverage maps (mapping tasks), among others",
+          TranslWork17: "Exposition to network infrastructure and concepts such as GPON, OLTs and Logic Network Diagrams",
+          TranslWork18: "Present a tutorial to other staff members on new mapping functionalities",
+          TranslWork19: "Involvement in Charitable Campaign as an ambassador - organize fundraising activities to incite donations",
+
+          TranslWork20: "Additional Work Experience",
+          TranslWork21: "Groupe Vertdure Outaouais - Direct Seller",
+          TranslWork22: "Cooperate with other employees and divide areas to cover",
+          TranslWork23: "Cultivating leadership by assisting new employees",
+          TranslWork24: "Develop communication and persuasion skills by engaging with clients from different backgrounds and conversing with them strategically",
+          TranslWork25: "Best Buy - Technical Sales Specialist",
+          TranslWork26: "Understand how to communicate well with complex customers and know how to manage their dissatisfaction",
+          TranslWork27: "Develop teamwork skills by working with the rest of the team to provide support when requested via radio communication",
+          TranslWork28: "Persuade a reluctant customer to purchase the products and services offered in store",
+          TranslWork29: "Strengthen interpersonal skills by establishing strong connections with the customer to facilitate the seller-customer relationship",
+
+          TranslSkill1: "Skills",
+
+          TranslInt0: "Interests and Hobbies",
+          TranslInt1: "Calisthenics",
+          TranslInt2: "A healthy body leads to a healthy mind! While calisthenics can prove to be challenging, it is an engaging endeavor, deconstructed one step at a time. Recently, I learned how to hold the crow pose!",
+          TranslInt3: "Writing",
+          TranslInt4: "For me, language is an art, and without being pedantic, I enjoy exercising the full, untapped potential of the French and English vocabulary. So many unused words!",
+          TranslInt5: "SAE Formula Club",
+          TranslInt6: "Although my integration to this club is recent, I dove into as much documentation as I could, assimilating as many details as possible. I even took an automotive engineering course! I am currently in the electrical team.",
+          TranslInt7: "Learning Japanese<br>日本語を勉強する",
+          TranslInt8: "こんにちは! - I love learning about new languages. One of my goals is to be a polyglot, and to mark the beginning of my linguistic journey, I started learning Japanese. I know a few hundred kanjis now!",
+          TranslInt9: "Drawing",
+          TranslInt10: "I also enjoy drawing, a great way to materialize my inspirations. Spaceships are surprisingly easy to draw!",
+
+          TranslCont1: "Contact Me",
+          TranslCont2: "Email -><br>wrash014@uottawa.ca",
+          TranslCont3: "Cellphone -><br>819-918-3404",
+          TranslCont4: "Landline -><br>819-773-3085",
+          TranslCont5: 'Linkedin Profile -><br><a href="https://www.linkedin.com/in/walid-rashad-a644201b6?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3ByVJnXF2pR0G3CZvHJh4sJw%3D%3D"><img src="./Images/LinkedIn_Logo.png" alt="Linkedin Logo" height="40px"></a>',
+
+        },
+        fr: {
+          TRANSLintro1: "Salut, je suis Walid!",
+          TRANSLintro2: "En tant qu'étudiant de quatrième année à l'Université d'Ottawa, je me suis efforcé de diversifier mon éventail de compétences et j'ai touché à différents domaines, tels que le génie aérospatial, naval et des télécommunications. Je vous invite à consulter mon profil - c'est une extension de mon CV, et je l'espère, une version plus engageante de celui-ci.",
+          
+          TranslEduc1: "Éducation",
+          TranslEduc2: "Bien que ce lycée soit généralement réservé aux étudiants issus de familles aisées, j’ai été sélectionné parmi de nombreux autres candidats pour une bourse complète. Apparemment, j'avais réussi le concours et je suis devenu l'unique gagnant du prix. Au fil des années, j'ai accumulé de nombreuses mentions honorifiques en raison de mes notes, ainsi que d'autres récompenses pour ma participation à l'équipe de volleyball.",
+          TranslEduc3: "Université d'Ottawa",
+          TranslEduc4: "En raison de mes notes, j’ai eu la possibilité de sauter complètement le CÉGEP et de m’inscrire immédiatement dans une université de l’Ontario après l’obtention de mon diplôme. J'étudie depuis à l'Université d'Ottawa en tant qu'ingénieur électricien et j'ai pu vivre de nombreux stages différents.",
+
+          ///////////////////////
+          
+          TranslWork1: "Expérience de travail",
+
+          TranslWork2: "Garde Côtière Canadienne (GCC)",
+          TranslWork3: "La Garde côtière canadienne est un ministère fédéral qui supervise la conception et la conception de la flotte canadienne. Il vise à protéger l’écosystème maritime et la souveraineté navale du Canada.",
+          TranslWork4: "Aider à la construction et à la mise à jour de l'infrastructure électrique des navires",
+          TranslWork5: "Explorez des sujets de recherche autour des énergies renouvelables et des sources d'énergie potentielles pour les futurs navires",
+          TranslWork6: "Analyser et valider des documents officiels tout en découvrant de nouveaux concepts",
+          TranslWork7: "Discutez de diverses avancées dans une multitude de projets avec le groupe d'ingénierie de la GCC",
+                    
+          TranslWork8: "Bombardier Aviation est une entreprise leader sur le marché des jets privés, fabriquant des avions pour des clients de premier plan depuis des décennies. J'ai travaillé dans l'équipe Compatibilité Electromagnétique (CEM).",
+          TranslWork9: "Contribuer à la validation/développement d'outils pour simuler et analyser les données CEM",
+          TranslWork10: "Utilisez un logiciel CEM tel qu'Altair Feko pour simuler les interférences électromagnétiques entre les câbles et l'efficacité du blindage anti-interférence",
+          TranslWork11: "Analyser les données et tester les résultats des projets de recherche en cours",
+          TranslWork12: "Participer à des réunions et conférences de partage de connaissances avec l’équipe EMC",
+
+          TranslWork13: "Innovation, Science et Développement Économique (ISDE)",
+          TranslWork14: "ISDE est un autre ministère fédéral qui entreprend des tâches en matière de télécommunications, notamment le financement et la supervision de projets d'expansion d'Internet afin de desservir les régions et villages canadiens les plus éloignés.",
+          TranslWork15: "Introduction à VBA - modification et ajout de fonctions dans les outils de base de données dans Microsoft Excel/Access. J'ai également aidé d'autres personnes à déboguer et à résoudre des problèmes de codage.",
+          TranslWork16: "Revues de projets - livrables, sites de projets, cartes de couverture (tâches de cartographie), entre autres",
+          TranslWork17: "Exposition à l'infrastructure réseau et aux concepts tels que GPON, OLT et diagrammes de réseau logique",
+          TranslWork18: "Présenter un tutoriel aux autres membres du personnel sur les nouvelles fonctionnalités de cartographie",
+          TranslWork19: "Implication dans une campagne caritative en tant qu'ambassadeur - organiser des activités de collecte de fonds pour inciter aux dons",
+
+          TranslWork20: "Expérience de travail supplémentaire",
+          TranslWork21: "Groupe Vertdure Outaouais - Vendeur direct",
+          TranslWork22: "Coopérer avec les autres employés et diviser les zones à couvrir",
+          TranslWork23: "Cultiver le leadership en accompagnant les nouveaux employés",
+          TranslWork24: "Développer des compétences de communication et de persuasion en s'engageant avec des clients d'horizons différents et en conversant avec eux de manière stratégique",
+          TranslWork25: "Best Buy - Spécialiste des ventes techniques",
+          TranslWork26: "Comprendre comment bien communiquer avec des clients complexes et savoir gérer leur insatisfaction",
+          TranslWork27: "Développer les compétences de travail d'équipe en travaillant avec le reste de l'équipe pour fournir un soutien sur demande via la communication radio.",
+          TranslWork28: "Persuader un client réticent à acheter les produits et services proposés en magasin",
+          TranslWork29: "Renforcer les compétences relationnelles en établissant des liens solides avec le client pour faciliter la relation vendeur-client",
+
+          TranslSkill1: "Atouts",
+
+          TranslInt0: "Intérêts et passions",
+          TranslInt1: "Calisthénie",
+          TranslInt2: "Un corps sain mène à un esprit sain ! Même si la gymnastique suédoise peut s'avérer un défi, il s'agit d'une activité engageante, déconstruite étape par étape. Récemment, j'ai appris à tenir la pose du corbeau!",
+          TranslInt3: "Écriture",
+          TranslInt4: "Pour moi, la langue est un art et, sans être pédant, j'aime exercer tout le potentiel inexploité du vocabulaire français et anglais. Tant de mots inutilisés!",
+          TranslInt5: "Club de formule SAE",
+          TranslInt6: "Bien que mon intégration dans ce club soit récente, j'ai fouillé le plus de documentation possible, assimilant le plus de détails possible. J'ai même suivi un cours d'ingénierie automobile! Je suis actuellement dans l'équipe électrique.",
+          TranslInt7: "Apprendtissage du japonais<br>日本語を勉強する",
+          TranslInt8: "こんにちは! - J'aime apprendre de nouvelles langues. L'un de mes objectifs est d'être polyglotte, et pour marquer le début de mon parcours linguistique, j'ai commencé à apprendre le japonais. Je connais quelques centaines de kanjis maintenant!",
+          TranslInt9: "Dessin",
+          TranslInt10: "J'apprécie également le dessin, un excellent moyen de matérialiser mes inspirations. Les vaisseaux spatiaux sont étonnamment faciles à dessiner!",
+
+          TranslCont1: "Contactez-moi",
+          TranslCont2: "Courriel -><br>wrash014@uottawa.ca",
+          TranslCont3: "Cellulaire -><br>819-918-3404",
+          TranslCont4: "Téléphone fixe -><br>819-773-3085",
+          TranslCont5: 'Profil Linkedin -><br><a href="https://www.linkedin.com/in/walid-rashad-a644201b6?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3ByVJnXF2pR0G3CZvHJh4sJw%3D%3D"><img src="./Images/LinkedIn_Logo.png" alt="Linkedin Logo" height="40px"></a>',
+
+        }
+        // Add more languages as needed
+      };
+  
+      // Function to change language
+      function changeLanguage(languageCode) {
+        // Iterate through elements to translate
+        Object.keys(languages[languageCode]).forEach(key => {
+          const element = document.getElementById(key);
+          if (element) {
+            element.innerHTML = languages[languageCode][key];
+          }
+        });
+      }
